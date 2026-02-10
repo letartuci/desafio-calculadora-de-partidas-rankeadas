865fd82efbfd156f36f540efc9f5162f399da4eb
letartuci <leticiatartuci@gmail.com>
2026-02-10 17:37:11 -0300

    saldo de vitorias

---
+++ index.js
@@ -0,0 +1,35 @@
+// Função que calcula o saldo e o nível do herói
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
+}
+
+// Exemplo de uso (simulação de partidas)
+let vitorias = 85;
+let derrotas = 45;
+
+// Chamada da função
+let resultado = calcularNivel(vitorias, derrotas);
+
+// Saída final
+console.log(
+    `O Herói tem de saldo de ${resultado.saldoVitorias} vitórias e está no nível de ${resultado.nivel}`
+);
